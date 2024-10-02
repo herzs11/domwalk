9770be5e47966e23f0d041bab926a78e019699d9
Seth Herz <sherz@unum.com>
2024-10-02 18:24:53 -0400

    Working version with sync to bigquery

--- db/sqlite_conn.go
+++ db/sqlite_conn.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"os"
 	"sync"
 
 	"gorm.io/driver/sqlite"
@@ -11,8 +12,9 @@ var GormDB *gorm.DB
 var Mut *sync.Mutex
 
 func init() {
+	db_name := os.Getenv("GORM_SQLITE_NAME")
 	var err error
-	GormDB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
+	GormDB, err = gorm.Open(sqlite.Open(db_name), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
