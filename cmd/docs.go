88b69a3caf5519614f1f03d49c2a4aba244900a0
Seth Herz <sherz@unum.com>
2024-10-08 22:36:08 -0400

    Added documentation

--- cmd/docs.go
+++ cmd/docs.go
@@ -10,8 +10,7 @@ import (
 
 // docsCmd represents the docs command
 var docsCmd = &cobra.Command{
-	Use:   "docs",
-	Short: "A brief description of your command",
+	Use: "docs",
 	Run: func(cmd *cobra.Command, args []string) {
 		err := doc.GenMarkdownTree(rootCmd, "/Users/sherz/GolandProjects/domwalk/docs/")
 		if err != nil {
@@ -23,14 +22,4 @@ var docsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(docsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// docsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// docsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
